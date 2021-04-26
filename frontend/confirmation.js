35cd27e4beffa351529d247def622a31499e92df
kingofgame16 <pierrechabroux@gmail.com>
2021-04-26 11:10:11 +0200

    ajout order et price au html

---
+++ frontend/confirmation.js
@@ -0,0 +1,8 @@
+const totalPriceElement = document.getElementById('totalPrice');
+const orderIdElement = document.getElementById('orderId');
+
+const totalPrice = JSON.parse(localStorage.getItem('totalPrice'));
+const orderId = JSON.parse(localStorage.getItem('orderId'));
+
+totalPriceElement.innerText = totalPrice; 
+orderIdElement.innerText = orderId; 
